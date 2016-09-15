697defee59f0cb26b4209de7b0c59739108807af
Alexis Algoud <alexis@atm-consulting.fr>
2016-09-15 16:11:32 +0200

    test

---
+++ php-google-contacts-v3-api/authorise-application.php
@@ -0,0 +1,17 @@
+<?php
+
+// After filling in the clientID, clientSecret and redirectUri (within 'config.json'), you should visit this page
+// to get the authorisation URL.
+
+// Note that the redirectUri value should point towards a hosted version of 'redirect_handler.php'.
+
+require_once 'vendor/autoload.php';
+
+use rapidweb\googlecontacts\helpers\GoogleHelper;
+
+$client = GoogleHelper::getClient();
+
+$authUrl = GoogleHelper::getAuthUrl($client);
+
+echo 'Go to the following URL to authorise your application for Google Contacts: '.$authUrl;
+echo "\r\n";
---
+++ php-google-contacts-v3-api/helpers/GoogleHelper.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace rapidweb\googlecontacts\helpers;
+
+abstract class GoogleHelper
+{
+    private static function loadConfig()
+    {
+        $contents = file_get_contents(__DIR__.'/../.config.json');
+
+        $config = json_decode($contents);
+
+        return $config;
+    }
+
+    public static function getClient()
+    {
+        $config = self::loadConfig();
+
+        $client = new \Google_Client();
+
+        $client->setApplicationName('Rapid Web Google Contacts API');
+
+        $client->setScopes(array(/*
+        'https://apps-apis.google.com/a/feeds/groups/',
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://apps-apis.google.com/a/feeds/alias/',
+        'https://apps-apis.google.com/a/feeds/user/',*/
+        'https://www.google.com/m8/feeds/',
+        /*'https://www.google.com/m8/feeds/user/',*/
+        ));
+
+        $client->setClientId($config->clientID);
+        $client->setClientSecret($config->clientSecret);
+        $client->setRedirectUri($config->redirectUri);
+        $client->setAccessType('offline');
+        $client->setApprovalPrompt('force');
+        $client->setDeveloperKey($config->developerKey);
+
+        if (isset($config->refreshToken) && $config->refreshToken) {
+            $client->refreshToken($config->refreshToken);
+        }
+
+        return $client;
+    }
+
+    public static function getAuthUrl(\Google_Client $client)
+    {
+        return $client->createAuthUrl();
+    }
+
+    public static function authenticate(\Google_Client $client, $code)
+    {
+        $client->authenticate($code);
+    }
+
+    public static function getAccessToken(\Google_Client $client)
+    {
+        return json_decode($client->getAccessToken());
+    }
+}
