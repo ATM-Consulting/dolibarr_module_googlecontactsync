ba61fbd61df9ce336ead3269cbabd37d8062075a
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2018-07-10 09:56:24 +0200

    Update sync-all-contact-for-user.php

--- script/sync-all-contact-for-user.php
+++ script/sync-all-contact-for-user.php
@@ -17,15 +17,26 @@
 				  	
 	$PDOdb=new TPDOdb;
 
-	$Tab = $PDOdb->ExecuteAsArray("SELECT rowid FROM ".MAIN_DB_PREFIX."socpeople 
-		WHERE 1 ");
+	$Tab = $PDOdb->ExecuteAsArray("
+		SELECT rowid, 'contact' as type_object FROM ".MAIN_DB_PREFIX."socpeople WHERE 1 
+		UNION
+		SELECT rowid, 'societe' as type_object FROM ".MAIN_DB_PREFIX."societe WHERE 1 
+		UNION
+		SELECT rowid, 'user_object' as type_object FROM ".MAIN_DB_PREFIX."user WHERE 1 
+		
+	");
 echo count($Tab);
 //	var_dump($Tab);
 
 	foreach($Tab as &$row) {
 		$fk_object = $row->rowid;
-		$type_object = 'contact';
+		$type_object = $row->type_object;
 
+		if ($type_object == 'societe' && empty($conf->global->GCS_GOOGLE_SYNC_THIRDPARTY)) continue;
+		if ($type_object == 'contact' && (empty($conf->global->GCS_GOOGLE_SYNC_CONTACT) || !empty($conf->global->GCS_GOOGLE_SYNC_ALL_CONTACT_FROM_SOCIETE))) continue;
+		if ($type_object == 'user_object' && empty($conf->global->GCS_GOOGLE_SYNC_USER)) continue;
+		
+		
 			$token = new TGCSToken;
 			$token->loadByObject($PDOdb, $fk_object, $type_object, $fk_user);
 			$token->fk_object = $fk_object;
